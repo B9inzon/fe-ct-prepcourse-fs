f5a740d181b4179c2581f512784d1107ee650868
B9inzon <tatopinzon369@gmail.com>
2024-06-19 15:47:46 -0500

    c7 tests performed

--- C7 Arrays/ejercicios/04.js
+++ C7 Arrays/ejercicios/04.js
@@ -2,6 +2,7 @@ function agregarItemAlFinalDelArray(array, elemento) {
   // Agrega el "elemento" al final del arreglo recibido.
   // Retorna el arreglo.
   // Tu código:
+  return array.concat(elemento);
 }
 
 module.exports = agregarItemAlFinalDelArray;
